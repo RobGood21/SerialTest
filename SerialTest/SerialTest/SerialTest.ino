f82014e5346101010a82bbe6b3ae5f56e7f35bef
Rob Antonisse <robantonisse@gmail.com>
2021-06-30 09:04:40 +0200

    Start

---
+++ SerialTest/SerialTest/SerialTest.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		SerialTest.ino
+ Created:	6/30/2021 9:02:24 AM
+ Author:	Rob Antonisse
+
+ Diverse test met serial com poort en groeter displays.
+
+
+*/
+
+
+void setup() {
+
+}
+
+void loop() {
+  
+}
