06de1f1d6f1183d92cbab1400d18549b29b51e0d
Rob Antonisse <robantonisse@gmail.com>
2021-06-30 09:17:31 +0200

    Begin, display werkt.

--- SerialTest/SerialTest/SerialTest.ino
+++ SerialTest/SerialTest/SerialTest.ino
@@ -9,7 +9,26 @@
 */
 
 
+
+#include <TFT.h>
+#include <SPI.h>
+
+#define cs 10
+#define dc 9
+#define rst 8
+
+TFT TFTscreen = TFT(cs, dc, rst);
+
+
+
+
 void setup() {
+	TFTscreen.begin();
+	TFTscreen.background(0, 0, 0);
+	TFTscreen.stroke(255, 255, 255);
+	TFTscreen.setTextSize(2);
+
+	TFTscreen.text("Wisselmotor", 20, 20);
 
 }
 
